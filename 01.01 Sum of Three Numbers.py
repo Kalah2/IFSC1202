301970241f772e9dcfc0480edd30f8200a0f2626
Kalah2 <157841924+Kalah2@users.noreply.github.com>
2025-01-27 16:41:57 +0000

    examples

---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,6 @@
+firstnumber = input("First Number: ")# first and second and third numbers can be x,y or something shorter
+secondnuber = input("Second Number: ")
+thirdnumber = input("Third Number: ")
+sum = int(firstnumber) + int(secondnuber) + int(thirdnumber)
+print(sum)
+
---
+++ 01.02 Hello.py
@@ -0,0 +1,3 @@
+name = input("First Name: ")
+print("Hello" + name)
+
