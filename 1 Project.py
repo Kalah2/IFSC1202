715c9935e48d91da32f57d85a6e3733440b2b27c
Kalah2 <157841924+Kalah2@users.noreply.github.com>
2025-02-05 16:26:13 +0000

    edit to project 1 and class work

--- 1 Project.py
+++ 1 Project.py
@@ -1,18 +1,23 @@
-seconds_per_day = float(input("Enter Length of Time in Seconds: "))
-seconds_per_year = 360*24*60*60
-year = float(input(seconds/seconds_per_year))
-seconds_left = float(input(seconds%seconds_per_year))
-seconds_per_day = 24*60*60
-days = float(input(seconds_left/seconds_per_day))
-seconds_left = float(input(seconds_left%seconds_per_day))
-seconds_per_hour = 60*60
-hours = float(input(seconds_left/seconds_per_hour))
-seconds_left = float(input(seconds_left%seconds_per_hour))
-minutes = float(input(seconds_left/60))
-seconds = float(input(seconds_left%60))
+seconds = int(input("Enter Length of Time in Seconds: "))
 
-print("years: " + year)
-print("days: " + days)
-print("hours: " + hours)
-print("minutes: " + minutes)
-print("seconds: " + seconds)
+seconds_per_year = 365*24*60*60
+year = seconds // seconds_per_year
+seconds = seconds - (year * seconds_per_year)
+
+seconds_per_day = 24*60*60
+days = seconds // seconds_per_day
+seconds = seconds - (days * seconds_per_day)
+
+seconds_per_hour = 60*60
+hours = seconds // seconds_per_hour
+seconds = seconds - (hours*seconds_per_hour)
+
+seconds_per_minute = 60
+minutes = seconds // seconds_per_minute
+seconds = seconds - (minutes * seconds_per_minute)
+
+print("years: ", year)
+print("days: " , days)
+print("hours: ", hours)
+print("minutes: ", minutes)
+print("seconds: ", seconds)
